f64d79bc85a8b053e11e2e6e8c39c2a939bd47e1
brennaveir <brennaveir@hotmail.com>
2023-06-17 15:06:05 -0400

    added add page with test content

---
+++ src/pages/add.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const Add = () => {
+    return (
+        <Layout>
+            <h1>HI!</h1>
+        </Layout>
+    )
+}
+
+export default Add;
