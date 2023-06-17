75f2bee80b45ec865022fb5775f80de95dacc0b8
brennaveir <brennaveir@hotmail.com>
2023-06-17 15:02:53 -0400

    added navbar and test content to home and collection pages

---
+++ src/components/layout.js
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Layout( {children} ) {
+  return (
+    <>
+    <Navbar bg="primary" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand href="/">Compendium Companion</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/collection">My Collection</Nav.Link>
+        <Nav.Link href="/add">Add</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+ <main>
+    {children}
+ </main>
+ </>
+  );
+}
+
+export default Layout;
