da1de9ddcf191757c46ece4d47066d3c532681b7
brennaveir <brennaveir@hotmail.com>
2023-06-25 20:29:06 -0400

    fixed image query error by moving images into the collection folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
+   
     resolve: 'gatsby-source-filesystem',
     options: {
       name: `collection`,
--- src/pages/collection.js
+++ src/pages/collection.js
@@ -1,14 +1,20 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Table from 'react-bootstrap/Table';
+import { graphql } from 'gatsby'
 
-const Collection = () => {
+const Collection = ({ data }) => {
+  
     return (
         <>
         <Layout pageTitle="Collection">
-            <Table striped bordered hover>
+          
+          {
+            data.allMdx.nodes.map((node) => (
+  <Table striped bordered hover>
       <thead>
         <tr>
+          <th>pic here</th>
           <th>Outfit Piece</th>
           <th>Upgrade 1</th>
           <th>Upgrade 2</th>
@@ -16,20 +22,37 @@ const Collection = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr key={node.id}>
           <td></td>
-          <td></td>
+          <td>{node.frontmatter.title}</td>
+          <td>{node.excerpt}</td>
           <td></td>
           <td></td>
         </tr>
 
       </tbody>
     </Table>
+            ))
+          }
+          
         </Layout>
         </>
     )
 }
 
+export const query = graphql`
+  query {
+    allMdx(sort: {frontmatter: {title: ASC}}) {
+    nodes {
+      frontmatter {
+        title
+      }
+      id
+      excerpt
+    }
+  }
+}
+`
 export const Head = () => <title>Collection</title>
 
 export default Collection
